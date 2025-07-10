e2c77182be210dab74cbe5840c29ca4e769b515f
Rana <reduanulislam92665@gmail.com>
2025-07-10 07:54:03 +0600

    javascript added

---
+++ store.js
@@ -0,0 +1,41 @@
+
+
+function handleSearch(){
+      const searchInputElement = document.getElementById('search-input-field');
+      const searchInputValue = searchInputElement.value;
+      console.log(searchInputValue);
+      loadPhone(searchInputValue);
+}
+
+const loadPhone = async(searchText)=>{
+ const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+ const serverData = await res.json();
+ displayPhone(serverData.data)
+}
+
+
+const displayPhone = (data)=>{
+      const cardContainer = document.getElementById("card-section");
+      cardContainer.innerHTML = "";
+      data.forEach(siglePhone => {
+            const productCard = document.createElement("div");
+            productCard.classList.add("card");
+            productCard.innerHTML = `
+            <div class="card-image">
+                  <img src="${siglePhone.image}" alt="phone-img">
+            </div>
+            <h3 class="card-title">${siglePhone.phone_name}</h3>
+            <p class="card-description">
+                  There are many variations of passages of available, but the majority have suffered
+            </p>
+            <div class="card-price">
+                  <span>$</span>
+                  <span class="card-price-price">${siglePhone.slug}</span>
+            </div>
+            <div class="card-button">
+                  <button class="btn">Show Details</button>
+            </div>
+            `;
+            cardContainer.appendChild(productCard);
+      });
+}
